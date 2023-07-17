e532c2e9c40bce96db57509b9f20d41b82b05982
Nachiket <jadhao1044@gmail.com>
2023-07-17 20:25:54 +0530

    first commit

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -35,20 +35,20 @@ public class Vehicle {
         this.name = name;
     }
 
-    public void steer(int direction){
-        //direction is in degrees, add it to the current direction
+    public void steer(int direction) {
+        // direction is in degrees, add it to the current direction
         setCurrentDirection(direction);
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
-    public void move(int speed, int direction){
-        setCurrentSpeed(speed);
-        setCurrentDirection(direction);
-        //set the values of currentSpeed and currentDirection
-        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
+    public void move(int speed, int direction) {
+        move(speed, direction);
+        // set the values of currentSpeed and currentDirection
+        System.out.println("move method called - The speed is changed to: " + currentSpeed
+                + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
-    public void stop(){
+    public void stop() {
         setCurrentSpeed(0);
         System.out.println("stop method called - The vehicle is stopped");
     }
